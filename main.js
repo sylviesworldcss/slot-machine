6b8ecc2735263672e8c608049e4f596653fce0f5
Sylvie Barjolo <144256707+sylviesworldcss@users.noreply.github.com>
2025-09-23 13:45:45 -0400

    citation changes

---
+++ main.js
@@ -0,0 +1,91 @@
+let act1 = document.querySelectorAll(".theAct1 p");
+
+let showMoney = document.querySelector(".theAct2");
+
+let miniButton = document.querySelector("#miniButton");
+miniButton.addEventListener('click', miniShuffle);
+let maxiButton = document.querySelector("#maxiButton")
+maxiButton.addEventListener('click', maxiShuffle);
+
+let startingFunds = 1600;
+let balance = document.querySelector("#balance");
+balance.innerText = startingFunds;
+
+function dance(bet) {
+  if (bet > startingFunds) {
+    alert("Put The Coco Chanel Down, Brokie!");
+    return false;
+  }
+  return true;
+}
+
+function miniShuffle() {
+    let bet = 40;
+    if(dance(bet) === false) {
+        return;
+    }
+    startingFunds = startingFunds - bet;
+    balance.innerText = startingFunds;
+
+    if(startingFunds<=0) {
+      document.querySelector("#hereWeGo1").innerText = "MOVE OUT THE WAY, Brokie!";
+      miniButton.disabled = true;
+      maxiButton.disabled = true;
+      return;
+    }
+    
+    let hereWeGo1 = Math.floor(Math.random()* 5);
+    document.querySelector("#hereWeGo1").innerText = hereWeGo1;
+
+    let hereWeGo2 = Math.floor(Math.random()* 5);
+    document.querySelector("#hereWeGo2").innerText = hereWeGo2;
+
+    let hereWeGo3 = Math.floor(Math.random()* 5);
+    document.querySelector("#hereWeGo3").innerText = hereWeGo3;
+
+    if(hereWeGo1 === hereWeGo2 && hereWeGo2 === hereWeGo3) {
+        let winningTotal = bet * 25;
+        startingFunds +=winningTotal;
+        balance.innerText = startingFunds;
+        alert(`THIQUE MONEY WON $${winningTotal}!`);
+    }
+    }
+
+function maxiShuffle() {
+    let bet = 400;
+    if(dance(bet) === false) {
+        return;
+    }
+    startingFunds = startingFunds - bet;
+    balance.innerText = startingFunds;
+
+    if(startingFunds<=0) {
+      document.querySelector("#hereWeGo1").innerText = "MOVE OUT THE WAY, Brokie!";
+      miniButton.disabled = true;
+      return;
+    }
+ 
+    let hereWeGo1 = Math.floor(Math.random()* 5);
+    document.querySelector("#hereWeGo1").innerText = hereWeGo1;
+
+    let hereWeGo2 = Math.floor(Math.random()* 5);
+    document.querySelector("#hereWeGo2").innerText = hereWeGo2;
+
+    let hereWeGo3 = Math.floor(Math.random()* 5);
+    document.querySelector("#hereWeGo3").innerText = hereWeGo3;
+
+    if(hereWeGo1 === hereWeGo2 && hereWeGo2 === hereWeGo3) {
+        let winningTotal = bet * 40;
+        startingFunds +=winningTotal;
+        balance.innerText = startingFunds;
+        alert(`THIQUE MONEY WON $${winningTotal}!`);
+
+    }
+}
+
+
+
+
+
+//  above code inspired by @leonnoel class clolor picker template, @xsarayu, @CodingWCal, @henryamatsu, @jorge0510  @TheophilusNdukwe 
+//  All had different js and I used google and gemini to explain what was what. For the functionality of this project I used the mixture for the structure.
